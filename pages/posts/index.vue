9ae45a6a9d6a50e44a0cb0ba395884ba03c03b95
YoProgrammer <yuttasakcom@gmail.com>
2018-02-13 18:03:46 +0700

    create store.js

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,22 +1,44 @@
 <template>
     <div class="posts-page">
-        <PostList />
+        <PostList :posts="loadedPosts" />
     </div>
 </template>
 
 <script>
-    import PostList from '@/components/Posts/PostList'
-    export default {
-        components: {
-            PostList
-        }
-    }
+import PostList from '@/components/Posts/PostList'
+export default {
+  components: {
+    PostList
+  },
+  asyncData(ctx, cb) {
+    setTimeout(() => {
+      cb(null, {
+        loadedPosts: [
+          {
+            id: '1',
+            title: 'First Post',
+            previewText: 'P text 1',
+            thumbnailLink: 'https://nuxtjs.org/nuxt-views-schema.png',
+            author: 'YoProgrammer'
+          },
+          {
+            id: '2',
+            title: 'Second Post',
+            previewText: 'P text 2',
+            thumbnailLink: 'https://nuxtjs.org/nuxt-views-schema.png',
+            author: 'YoProgrammer'
+          }
+        ]
+      })
+    }, 1000)
+  }
+}
 </script>
 
 <style scoped>
-    .posts-page {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
