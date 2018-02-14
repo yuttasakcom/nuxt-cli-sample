4e1c57f1ddb652538f3a5e877d5bb5a0e8c2d052
YoProgrammer <yuttasakcom@gmail.com>
2018-02-14 11:32:49 +0700

    update store

--- store/index.js
+++ store/index.js
@@ -88,19 +88,15 @@ const createStore = () => {
             localStorage.setItem('token', result.idToken)
             localStorage.setItem(
               'tokenExpiration',
-              new Date() + result.expiresIn * 1000
+              new Date().getTime() + Number.parseInt(result.expiresIn) * 1000
             )
             Cookie.set('jwt', result.idToken)
-            Cookie.set('expirationDate', new Date() + result.expiresIn * 1000)
-
-            vuexContext.dispatch('setLogoutTimer', result.expiresIn * 1000)
+            Cookie.set(
+              'expirationDate',
+              new Date().getTime() + Number.parseInt(result.expiresIn) * 1000
+            )
           })
           .catch(e => console.log(e))
-      },
-      setLogoutTimer(vuexContext, duration) {
-        setTimeout(() => {
-          vuexContext.commit('clearToken')
-        }, duration)
       },
       initAuth(vuexContext, req) {
         let token
@@ -109,7 +105,7 @@ const createStore = () => {
           if (!req.headers.cookie) {
             return
           }
-          const jwtCookie = req.headers.ookie
+          const jwtCookie = req.headers.cookie
             .split(';')
             .find(c => c.trim().startsWith('jwt='))
           if (!jwtCookie) {
@@ -117,23 +113,20 @@ const createStore = () => {
           }
           token = jwtCookie.split('=')[1]
 
-          expirationDate = req.headers.ookie
+          expirationDate = req.headers.cookie
             .split(';')
             .find(c => c.trim().startsWith('expirationDate='))
             .split('=')[1]
         } else {
           token = localStorage.getItem('token')
           expirationDate = localStorage.getItem('tokenExpiration')
-
-          if (new Date().getTime() > +expirationDate || !token) {
-            return
-          }
         }
 
-        vuexContext.dispatch(
-          'setLogoutTimer',
-          +expirationDate - new Date().getTime()
-        )
+        if (new Date().getTime() > +expirationDate || !token) {
+          vuexContext.commit('clearToken')
+          return
+        }
+
         vuexContext.commit('setToken', token)
       }
     },
