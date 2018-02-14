e5f20a2a31183ab2d9a220f71d4088973a82a5ae
YoProgrammer <yuttasakcom@gmail.com>
2018-02-14 11:44:26 +0700

    create logout

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,6 +2,7 @@
     <div class="admin-page">
         <section class="new-post">
             <AppButton @click="$router.push('/admin/new-post')">Create Post</AppButton>
+            <AppButton @click="onLogout" style="margin-left:10px">Logout</AppButton>
         </section>
 
         <section class="existing-posts">
@@ -18,6 +19,12 @@ export default {
   computed: {
     loadedPosts() {
       return this.$store.getters.loadedPosts
+    }
+  },
+  methods: {
+    onLogout() {
+      this.$store.dispatch('logout')
+      this.$router.push('/admin/auth')
     }
   }
 }
--- store/index.js
+++ store/index.js
@@ -123,11 +123,20 @@ const createStore = () => {
         }
 
         if (new Date().getTime() > +expirationDate || !token) {
-          vuexContext.commit('clearToken')
+          vuexContext.dispatch('logout')
           return
         }
 
         vuexContext.commit('setToken', token)
+      },
+      logout(vuexContext) {
+        vuexContext.commit('clearToken')
+        if (process.client) {
+          localStorage.removeItem('token')
+          localStorage.removeItem('tokenExpiration')
+        }
+        Cookie.remove('jwt')
+        Cookie.remove('expirationDate')
       }
     },
     getters: {
